7d90b62ebae9d81f2f05fca999d9bece41f599ab
Meka <ezemikey12@gmail.com>
2026-07-01 12:19:56 -0400

    logic in a main function, add bucket existence check, and handle S3 errors.

--- test_connection.py
+++ test_connection.py
@@ -3,12 +3,27 @@ from dotenv import load_dotenv
 from minio import Minio
 
 load_dotenv()
-client = Minio(
+
+def main():
+    client = Minio(
     os.getenv("MINIO_ENDPOINT"),
     access_key = os.getenv("MINIO_ACCESS_KEY"),
     secret_key = os.getenv("MINIO_SECRET_KEY"),
     secure = False      
 )
+    source_file = "/tmp/test.txt"
+    bucket_name = "test-bucket"
+    destination_file = "test.txt"
+    
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+        print(f"{bucket_name} created")
+    else:
+        print(f"{bucket_name} already exists")
+    # buckets = client.list_buckets()
 
-buckets = client.list_buckets()
-print("Connected. Buckets:", [bucket.name for bucket in buckets])
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as e:
+        print("Error:", "An Error Occured")
