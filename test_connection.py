5db7624c1b845f17b6a22b0f38af5405e8a30203
Meka <ezemikey12@gmail.com>
2026-07-01 12:48:28 -0400

    adding S3 error handling, bucket creation logic, and uploading functionality

--- test_connection.py
+++ test_connection.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from minio import Minio
+from minio.error import S3Error
 
 load_dotenv()
 
@@ -15,15 +16,19 @@ def main():
     bucket_name = "test-bucket"
     destination_file = "test.txt"
     
+    # create bucket if it doesn't exist
     if not client.bucket_exists(bucket_name):
         client.make_bucket(bucket_name)
         print(f"{bucket_name} created")
     else:
         print(f"{bucket_name} already exists")
+        
+    client.fput_object(bucket_name, destination_file, source_file)
+    print(f"{source_file}succesfully uploaded as {destination_file} uploaded to bucket {bucket_name}")
     # buckets = client.list_buckets()
 
 if __name__ == "__main__":
     try:
         main()
     except S3Error as e:
-        print("Error:", "An Error Occured")
+        print("Error:", {e})
